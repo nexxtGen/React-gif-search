cafad13559408c93bf073e460c982375f711c959
Kamil Szczypior <kszczypior1@gmail.com>
2018-09-29 06:54:46 +0200

    Change http request to Promises

--- components/App.js
+++ components/App.js
@@ -1,5 +1,3 @@
-const GIPHY_API_URL = 'https://api.giphy.com';
-const GIPHY_PUB_KEY = 'BuoPreQVJiLyBuCkEfhI26SyRpb3mwHo';
 
 App = React.createClass({
 
@@ -9,8 +7,8 @@ App = React.createClass({
             searchingText: '',
             gif: {}
         };
-    },
-
+    },    
+    /*
     handleSearch: function(searchingText) {  // 1. Pobierz na wejściu wpisywany tekst.
         this.setState({
           loading: true  // 2.Zasygnalizuj, że zaczął się proces ładowania.
@@ -23,9 +21,58 @@ App = React.createClass({
           });
         }.bind(this)); // Metoda bind pozwala zachować kontekst przekazywanej funkcji do mtody getGif
     },
+    */
+    handleSearch: function(searchingText) {  
+        this.setState({
+          loading: true  
+        });
+        this.getGif(searchingText)   
+            .then(gif => {                
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                });                  
+            })
+            .catch(error => {
+                console.log('Error log:', error);
+                this.setState({
+                    loading: false,
+                    gif: { url: './404.png', sourceUrl: './404.png' },
+                    searchingText: searchingText
+                });                                   
+            });       
+    },
 
     //metoda getGif
-
+    getGif: function(searchingText) {
+        const GIPHY_API_URL = 'https://api.giphy.com';
+        const GIPHY_PUB_KEY = 'BuoPreQVJiLyBuCkEfhI26SyRpb3mwHo';
+        const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        return new Promise(
+            (resolve, reject) => {                
+                const request = new XMLHttpRequest();
+                request.onload = function() {
+                    if (this.status === 200) {
+                        const data = JSON.parse(request.responseText).data; 
+                        const gif = {  
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        }
+                        resolve(gif);
+                    } else {                        
+                        reject(new Error(this.statusText));
+                    }
+                };
+                request.onerror = function() {
+                    reject(new Error(
+                        `XMLHttpRequest Error: ${this.statusText}`));
+                };
+                request.open('GET', url);
+                request.send(); 
+        });
+    },
+    /*    
     getGif: function(searchingText, callback) {  // 1. Na wejście metody getGif przyjmujemy dwa parametry: wpisywany tekst (searchingText) i funkcję, która ma się wykonać po pobraniu gifa (callback).
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2. Konstruujemy adres URL dla API Giphy (pełną dokumentację znajdziesz pod tym adresem).
         var xhr = new XMLHttpRequest();  // 3. Wywołujemy całą sekwencję tworzenia zapytania XHR do serwera i wysyłamy je.
@@ -51,7 +98,7 @@ App = React.createClass({
         }
         xhr.send();
     },
-
+    */
     render: function() {
 
         var styles = {
